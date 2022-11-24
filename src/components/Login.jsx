93e5a3369ce18dee76013b3143a8d6b713717c31
Terogene Claudio <cterogene@gmail.com>
2022-11-23 20:16:51 -0500

    added the env file for storing the id from google. and received github object for user login succesfully.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,13 @@ import { FcGoogle } from 'react-icons/fc';
 import ShareVideo from '../assets/share.mp4';
 import Logo from '../assets/logowhite.png';
 const Login = () => {
+
+  const responseGoogle = (response) =>{
+    console.log(response);
+  }
+
   return (
-    <GoogleOAuthProvider clientId='427207223029-m5jf54duk3jf0atkorghvsfiiuigequ2.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
       <div className='flex justify-start items-center flex-col h-screen'>
         <div className='relative w-full h-full'>
           <video
@@ -21,18 +26,22 @@ const Login = () => {
           />
 
           <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
-            <img src={Logo} width="130px" alt="logo" />
-          </div>
-          <div className='shadow-2xl'>
+            <img src={Logo} width="130px" alt="logo" className='mb-5' />
             <GoogleLogin
               clientId=""
               render={(renderProps) => (
-                <button type="button" className='bg-mainColor flex justify-center'>
-                  <FcGoogle className='mr-4' /> Sign in with Goodle
+                <button type="button" 
+                className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay m-5'
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                >
+                  <FcGoogle  /> Sign in with google
                 </button>
               )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin "
             />
-
           </div>
         </div>
       </div>
