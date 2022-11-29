e6f25aeafa2c1773a49bcbaa08eafa46d05d56b7
Terogene Claudio <cterogene@gmail.com>
2022-11-29 09:39:02 -0500

    customized the Sidebar with full screen and middle screen property

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -6,11 +6,23 @@ import {sidebar, userProfile} from '../components';
 import { client } from '../client';
 import logo from '../assets/logo.png';
 import Pins from './Pins';
+import Sidebar from '../components/Sidebar';
 
 const Home = () => {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
   return (
-    <div>
-      Home
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
+      <div className='hidden md:flex h-screen flex-initial'>
+        <Sidebar />
+      </div>
+      <div className='flex md:hidden flex-row'>
+        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(false)}/>
+        <Link to='/' >
+          <img  src={logo} alt='logo' className='w-28'/>
+        </Link>
+      </div>
     </div>
   )
 }
